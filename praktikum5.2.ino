15f2985ca1c2afd13bea1b80018bbbace054973d
mhanif57 <m.hanief97@gmail.com>
2016-10-10 10:58:29 +0700

    Add files via upload

---
+++ praktikum5.2.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  Serial.write(0x00);
+  Serial.write(0x01);
+  Serial.write(0xf0);
+  Serial.write(0x1f);
+  Serial.write(0x2f);
+  delay(2000);
+}
